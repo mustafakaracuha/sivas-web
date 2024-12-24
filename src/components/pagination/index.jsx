58042ad2520d24d8dc0eb94d942a86920750fb1e
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2024-12-24 10:06:18 +0300

    edit pagination

--- src/components/pagination/index.jsx
+++ src/components/pagination/index.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setIsVisible(true), 450); // Animasyon başlama süresi
+        return () => clearTimeout(timer); // Temizlik işlemi
+    }, []);
+
     const pageNumbers = Array.from({ length: totalPages }, (_, index) => index + 1);
 
     return (
-        <div className="w-full flex items-center justify-center fixed bottom-10 -left-0">
-            {/* Sol Ok */}
+        <div className={`fixed inset-x-0 bottom-10 flex items-center justify-center transition-opacity duration-500 ${isVisible ? "opacity-100" : "opacity-0"}`}>
             <button
                 onClick={() => onPageChange(currentPage > 1 ? currentPage - 1 : 1)}
                 className="px-3 py-2 mx-1 rounded-full bg-white text-gray-500 hover:bg-gray-200 disabled:opacity-50"
@@ -14,7 +20,6 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
                 ←
             </button>
 
-            {/* Sayfa Numaraları */}
             {pageNumbers.map((number) => (
                 <button
                     key={number}
@@ -25,7 +30,6 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
                 </button>
             ))}
 
-            {/* Sağ Ok */}
             <button
                 onClick={() => onPageChange(currentPage < totalPages ? currentPage + 1 : totalPages)}
                 className="px-3 py-2 mx-1 rounded-full bg-white text-gray-500 hover:bg-gray-200 disabled:opacity-50"
